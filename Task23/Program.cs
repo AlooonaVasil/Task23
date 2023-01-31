87ebd03e4d7b0702b9030ff7a1a86e8bb33d311e
Алёна Алёна <alenchaeto1@outlook.com>
2023-01-31 15:39:06 +0300

    Добавьте файлы проекта.

---
+++ Task23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(n);
+            Console.ReadKey();
+        }
+
+        static void Factorial(int n)
+        {
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+                Thread.Sleep(100);
+            }
+            Console.WriteLine($"Факториал = {factorial}");
+        }
+
+        static async void FactorialAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+    }
+}
