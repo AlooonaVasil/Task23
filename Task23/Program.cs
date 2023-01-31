7c1251d713801b2454bd3ea9473926e0eb7792b5
Алёна Алёна <alenchaeto1@outlook.com>
2023-01-31 15:48:05 +0300

    fix. добавила проверку

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,7 +15,23 @@ namespace Task23
         {
             Console.WriteLine("Введите n");
             int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
             FactorialAsync(n);
+
+            Console.WriteLine("Факториал последовательность");
+            for (int i = 0; i < n; i++)
+            {
+                sum += 1;
+                if (i == n - 1)
+                {
+                    Console.Write($"{sum}");
+                }
+                else
+                {
+                    Console.Write($"{sum} * ");
+                }
+
+            }
             Console.ReadKey();
         }
 
@@ -25,7 +43,7 @@ namespace Task23
                 factorial *= i;
                 Thread.Sleep(100);
             }
-            Console.WriteLine($"Факториал = {factorial}");
+            Console.WriteLine($" = {factorial}");
         }
 
         static async void FactorialAsync(int n)
